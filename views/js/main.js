81302aed24663e845435fcec0baded7c808744e0
Michael Cardenas <michael.cardenas@realtechltda.com>
2024-08-09 10:27:39 -0500

    calendario 2

--- views/js/main.js
+++ views/js/main.js
@@ -629,7 +629,7 @@ $("#calendario").click(function() {
               sessionStorage.setItem('calendarioData', JSON.stringify(response.data));
 
               // Ahora, redirigir a la vista:
-              window.location.href = "views/calendario/calendario.php";
+              window.location.href = "calendario/calendario.php";
             } else {
               alert(response.message);
           }
