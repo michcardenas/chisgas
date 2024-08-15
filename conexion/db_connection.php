5dbd16a37f0a632560dc621124fd350dad2108bf
Michael Cardenas <michael.cardenas@realtechltda.com>
2024-08-15 18:05:01 -0500

    hora

--- conexion/db_connection.php
+++ conexion/db_connection.php
@@ -4,6 +4,7 @@ $usuario = "root";
 $contraseña = "";  
 $basedatos = "chisgas";
 $puerto = 3306; 
+date_default_timezone_set('America/Bogota');
 
 $conn = new mysqli($servidor, $usuario, $contraseña, $basedatos, $puerto);
 
