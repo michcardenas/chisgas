e3f345a76adffe07a72fb79135b915e20b5f45a8
Samuel <sambarbosa66@gmail.com>
2024-08-30 18:51:13 -0500

    .

--- views/login/validar.php
+++ views/login/validar.php
@@ -9,7 +9,6 @@ include_once '../../conexion/db_connection.php';
 $statement = $conn->prepare('SELECT * FROM usuarios WHERE login = ?');
 
 if (!$statement) {
-    // Log the error
     die("Database query failed.");
 }
 
@@ -18,20 +17,24 @@ $statement->execute();
 $result = $statement->get_result();
 
 if ($result->num_rows === 0) {
-    // Log the error
-    header("Location: ../login/login.php?error=1");
+    header("Location: ../login/login.php?error=1"); // Usuario no encontrado
     exit();
 }
 
 $user = $result->fetch_assoc();
 
+// Verificar si la contraseña es correcta
 if ($password === $user['contrasena']) {
+    // Guardar la información del usuario en la sesión
     $_SESSION['username'] = $username;
     $_SESSION['grupo_usuario'] = $user['grupo_usuario'];
+    $_SESSION['id'] = $user['id']; // Asegúrate de que esta clave es correcta
+
     header('Location: ../menu.php');
     exit();
 } else {
-    header("Location: ../login/login.php?error=2");
+    header("Location: ../login/login.php?error=2"); // Contraseña incorrecta
     exit();
 }
+
 ?>
